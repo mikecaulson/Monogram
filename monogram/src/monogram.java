f6ffe802d727450fd620e56ee98d5a3dc4e22f6d
Michael.Caulson <Michael.Caulson@MU-C1-18.mullen.pvt.k12.co.us>
2016-05-02 10:20:38 -0600

    initial commit

---
+++ monogram/src/monogram.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class monogram
+{
+
+  public static void main(String [] args)
+	  {
+    
+    Scanner mono = new Scanner(System.in);
+    
+    String first, middle, last;
+
+    System.out.println("This is a program to generate a monogram.");
+    System.out.println("What is your first name? ");
+    first = mono.next();
+    System.out.println("What is your middle name? ");
+    middle = mono.next();
+    System.out.println("What is your last name? ");
+    last = mono.next();
+    
+    String initials = first.substring(0,1).toUpperCase() + last.substring(0,1).toUpperCase() + middle.substring(0,1).toUpperCase();
+    
+    System.out.println("Your monogram is: " + initials);
+    }
+}
